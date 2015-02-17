ce8bde110652ad0962c3789489512e49f1d18f8f
Ruiwen.Zhao <zhaorw93@gmail.com>
2015-02-17 20:44:23 +0800

    Update: Add new event, store events. But cannot delete yet.

---
+++ app/src/main/java/com/mycompany/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,79 @@
+package com.mycompany.myfirstapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class DisplayMessageActivity extends Activity {
+
+    private ListView lv;
+
+    public final static String EVENT = "com.mycompany.myfirstapp.EVENT";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_display_message, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, MainActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+
+        SharedPreferences sharedPref = getSharedPreferences("eventsFile", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        System.out.println("Here: " + message);
+
+        if(!message.isEmpty()) {
+            int numOfNotes = sharedPref.getInt("NumberOfNotes", 0);
+            editor.putInt("NumberOfNotes", numOfNotes + 1);
+            editor.commit();
+            editor.putString(Integer.toString(numOfNotes + 1), message);
+            editor.commit();
+        }
+
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/mycompany/myfirstapp/MainActivity.java
+++ app/src/main/java/com/mycompany/myfirstapp/MainActivity.java
@@ -1,29 +1,68 @@
 package com.mycompany.myfirstapp;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
 
-    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+
+    private ListView lv;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv = (ListView) findViewById(R.id.lv);//得到ListView对象的引用
+
+
+        SharedPreferences sharedPref = getSharedPreferences("eventsFile", Context.MODE_PRIVATE);
+        int numOfNotes = sharedPref.getInt("NumberOfNotes", 0);
+
+        //ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String, Object>>();
+
+        List<String> listItems = new ArrayList<>();
+
+        for(int i = 1; i <= numOfNotes; i++)
+        {
+            listItems.add(sharedPref.getString(Integer.toString(i), "WRONG!"));
+        }
+
+        lv.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, listItems));
+
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
         return true;
     }
 
@@ -32,26 +71,31 @@ public class MainActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                addEvent();
+                return true;
+            case R.id.action_settings:
+                //openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
-        return super.onOptionsItemSelected(item);
+     //   return super.onOptionsItemSelected(item);
     }
 
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
 
-        //put key-value pairs
-        intent.putExtra(EXTRA_MESSAGE, message);
+
+    public void addEvent() {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
         startActivity(intent);
     }
-
 }
